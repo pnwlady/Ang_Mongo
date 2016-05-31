116b3a09612ec07d7053d13f8f0014bfb9efdb26
heidilaursen <heidimlaursen@gmail.com>
2016-05-31 12:33:04 -0700

    first commit of refactor

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/build').listen(5000, () => {
+  process.stdout.write('server up on port 5000');
+}));
